3123f62d12e35ba883a15ddba912d903cd7def0a
jplck <pollack.jan@gmail.com>
2024-08-19 09:47:45 +0000

    refactor: Update SQL query tool to support SQL Server

--- src/sql-agents/app.py
+++ src/sql-agents/app.py
@@ -14,6 +14,32 @@ from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from promptflow.tracing import start_trace
 from bs4 import BeautifulSoup
 
+from typing import Annotated, Sequence, TypedDict
+
+from langchain_core.messages import BaseMessage
+
+from langgraph.graph.message import add_messages
+
+from typing import Annotated, Literal
+
+from langchain_core.messages import AIMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_openai import ChatOpenAI
+from typing_extensions import TypedDict
+
+from langgraph.graph import END, StateGraph, START
+from langgraph.graph.message import AnyMessage, add_messages
+
+from typing import Any
+
+from langchain_core.messages import ToolMessage
+from langchain_core.runnables import RunnableLambda, RunnableWithFallbacks
+from langgraph.prebuilt import ToolNode
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain_core.tools import tool
+from langchain_core.prompts import ChatPromptTemplate
+
 dotenv.load_dotenv()
 # start a trace session, and print a url for user to check trace
 start_trace()
@@ -93,18 +119,7 @@ odbc_str = 'mssql+pyodbc:///?odbc_connect=' \
                 'Driver='+driver+ \
                 ';' + os.getenv("AZURE_SQL_CONNECTIONSTRING")
 
-from langchain_community.utilities import SQLDatabase
-
 db = SQLDatabase.from_uri(odbc_str)
-print(db.dialect)
-print(db.get_usable_table_names())
-
-from typing import Any
-
-from langchain_core.messages import ToolMessage
-from langchain_core.runnables import RunnableLambda, RunnableWithFallbacks
-from langgraph.prebuilt import ToolNode
-
 
 def create_tool_node_with_fallback(tools: list) -> RunnableWithFallbacks[Any, dict]:
     """
@@ -128,21 +143,11 @@ def handle_tool_error(state) -> dict:
         ]
     }
 
-
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
-
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 tools = toolkit.get_tools()
 
 list_tables_tool = next(tool for tool in tools if tool.name == "sql_db_list_tables")
 get_schema_tool = next(tool for tool in tools if tool.name == "sql_db_schema")
-
-print(list_tables_tool.invoke(""))
-
-# print(get_schema_tool.invoke("Artist"))
-
-from langchain_core.tools import tool
-
 
 @tool
 def db_query_tool(query: str) -> str:
@@ -155,11 +160,6 @@ def db_query_tool(query: str) -> str:
     if not result:
         return "Error: Query failed. Please rewrite your query and try again."
     return result
-
-
-print(db_query_tool.invoke("SELECT TOP 3 * FROM Orders"))
-
-from langchain_core.prompts import ChatPromptTemplate
 
 query_check_system = """You are a SQL expert with a strong attention to detail.
 Double check the SQL Server query for common mistakes, including:
@@ -185,23 +185,6 @@ query_check = query_check_prompt | llm.bind_tools(
 )
 
 query_check.invoke({"messages": [("user", "SELECT TOP 3 * FROM Orders")]})
-
-from typing import Annotated, Sequence, TypedDict
-
-from langchain_core.messages import BaseMessage
-
-from langgraph.graph.message import add_messages
-
-from typing import Annotated, Literal
-
-from langchain_core.messages import AIMessage
-from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain_openai import ChatOpenAI
-from typing_extensions import TypedDict
-
-from langgraph.graph import END, StateGraph, START
-from langgraph.graph.message import AnyMessage, add_messages
-
 
 # Define the state for the agent
 class State(TypedDict):
@@ -256,33 +239,16 @@ workflow.add_node(
     },
 )
 
-
-# Describe a tool to represent the end state
-class SubmitFinalAnswer(BaseModel):
-    """Submit the final answer to the user based on the query results."""
-
-    final_answer: str = Field(..., description="The final answer to the user")
-
-
 # Add a node for a model to generate a query based on the question and schema
 query_gen_system = """You are a SQL expert with a strong attention to detail.
 
-Given an input question, output a syntactically correct SQL Server query to run, then look at the results of the query and return the answer.
-
-DO NOT call any tool besides SubmitFinalAnswer to submit the final answer.
+Given an input question, output a syntactically correct SQL Server query.
 
 When generating the query:
-
-Output the SQL query that answers the input question without a tool call.
 
 Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most 5 results.
 You can order the results by a relevant column to return the most interesting examples in the database.
 Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-
-If you get an error while executing a query, rewrite the query and try again.
-
-If you get an empty result set, you should try to rewrite the query to get a non-empty result set. 
-NEVER make stuff up if you don't have enough information to answer the query... just say you don't have enough information.
 
 If you have enough information to answer the input question, simply invoke the appropriate tool to submit the final answer to the user.
 
@@ -290,10 +256,7 @@ DO NOT make any DML statements (CREATE, INSERT, UPDATE, DELETE, DROP etc.) to th
 query_gen_prompt = ChatPromptTemplate.from_messages(
     [("system", query_gen_system), ("placeholder", "{messages}")]
 )
-query_gen = query_gen_prompt | llm.bind_tools(
-    [SubmitFinalAnswer]
-)
-
+query_gen = query_gen_prompt | llm
 
 def query_gen_node(state: State):
     message = query_gen.invoke(state)
@@ -322,18 +285,14 @@ workflow.add_node("correct_query", model_check_query)
 # Add node for executing the query
 workflow.add_node("execute_query", create_tool_node_with_fallback([db_query_tool]))
 
-
 # Define a conditional edge to decide whether to continue or end the workflow
-def should_continue(state: State) -> Literal["correct_query", "query_gen", "__end__"]:
+def should_continue(state: State) -> Literal["__end__", "query_gen"]:
     messages = state["messages"]
     last_message = messages[-1]
-    # If there is a tool call, then we finish
-    if getattr(last_message, "tool_calls", None):
-        return END
     if last_message.content.startswith("Error:"):
         return "query_gen"
     else:
-        return "correct_query"
+        return END
 
 
 # Specify the edges between the nodes
@@ -342,12 +301,12 @@ workflow.add_edge("first_tool_call", "list_tables_tool")
 workflow.add_edge("list_tables_tool", "model_get_schema")
 workflow.add_edge("model_get_schema", "get_schema_tool")
 workflow.add_edge("get_schema_tool", "query_gen")
+workflow.add_edge("query_gen", "correct_query")
+workflow.add_edge("correct_query", "execute_query")
 workflow.add_conditional_edges(
-    "query_gen",
+    "execute_query",
     should_continue,
 )
-workflow.add_edge("correct_query", "execute_query")
-workflow.add_edge("execute_query", "query_gen")
 
 # Compile the workflow into a runnable
 app = workflow.compile()
