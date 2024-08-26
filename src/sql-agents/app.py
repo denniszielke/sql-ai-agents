3dbefe7c11d43eceaa991c859e20ef3a09588ef2
jplck <pollack.jan@gmail.com>
2024-08-26 09:03:16 +0000

    refactor: Update app.py with opentelemetry tracing and session management

--- src/sql-agents/app.py
+++ src/sql-agents/app.py
@@ -35,13 +35,16 @@ from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod, NodeSt
 dotenv.load_dotenv()
 
 from opentelemetry import trace
-tracer = trace.get_tracer(__name__)
-
-# enable langchain instrumentation
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from openinference.instrumentation.langchain import LangChainInstrumentor
+from opentelemetry import trace as trace_api
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk import trace as trace_sdk
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry.instrumentation.langchain import LangchainInstrumentor
-instrumentor = LangchainInstrumentor()
-if not instrumentor.is_instrumented_by_opentelemetry:
-    instrumentor.instrument()
 
 st.set_page_config(
     page_title="AI agentic bot that can interact with a database"
@@ -49,6 +52,33 @@ st.set_page_config(
 
 st.title("💬 AI agentic RAG")
 st.caption("🚀 A Bot that can use an agent to retrieve, augment, generate, validate and iterate")
+
+@st.cache_resource
+def setup_tracing():
+    exporter = AzureMonitorTraceExporter.from_connection_string(
+        os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    )
+    tracer_provider = TracerProvider()
+    trace.set_tracer_provider(tracer_provider)
+    tracer = trace.get_tracer(__name__)
+    span_processor = BatchSpanProcessor(exporter, schedule_delay_millis=60000)
+    trace.get_tracer_provider().add_span_processor(span_processor)
+    LangchainInstrumentor().instrument()
+    return tracer
+
+def get_session_id() -> str:
+    id = random.randint(0, 1000000)
+    return "00000000-0000-0000-0000-" + str(id).zfill(12)
+
+@st.cache_resource
+def create_session(st: st) -> None:
+    if "session_id" not in st.session_state:
+        st.session_state["session_id"] = get_session_id()
+        print("started new session: " + st.session_state["session_id"])
+        st.write("You are running in session: " + st.session_state["session_id"])
+
+tracer = setup_tracing()
+create_session(st)
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
@@ -88,15 +118,6 @@ else:
         openai_api_type="azure_ad",
         streaming=True
     )
-
-def get_session_id() -> str:
-    id = random.randint(0, 1000000)
-    return "00000000-0000-0000-0000-" + str(id).zfill(12)
-
-if "session_id" not in st.session_state:
-    st.session_state["session_id"] = get_session_id()
-    print("started new session: " + st.session_state["session_id"])
-    st.write("You are running in session: " + st.session_state["session_id"])
 
 driver = '{ODBC Driver 18 for SQL Server}'
 odbc_str = 'mssql+pyodbc:///?odbc_connect=' \
